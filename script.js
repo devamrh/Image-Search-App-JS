f97b156dd20c6f94f286e95749cde0b4b108b917
A. M. Rafinul Huq <116501394+devamrh@users.noreply.github.com>
2023-07-30 03:25:39 +0600

    fifth commit

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let page = 1;
 async function searchImages() {
 
     inputData = inputEl.value;
-    const url = `http://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
     const response = await fetch(url);
     const data = await response.json();
